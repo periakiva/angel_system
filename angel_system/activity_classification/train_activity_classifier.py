23b70200a8cb4ee434f6208504b882f6ffcf9baa
Peri Akiva <peri.akiva@gyges.khq.kitware.com>
2024-01-29 07:04:27 -0500

    pose and offset feature vector fusion

--- angel_system/activity_classification/train_activity_classifier.py
+++ angel_system/activity_classification/train_activity_classifier.py
@@ -31,7 +31,7 @@ def data_loader(
 
     :return:
         - act_map: Activity label string to id dict
-        - inv_act_map: Activity id to label string dict
+        - inv_act_ma p: Activity id to label string dict
         - image_activity_gt: Image id to activity label string dict
         - image_id_to_dataset: Image id to id in ``dset`` dict
         - label_to_ind: Object detection labels to ids dict
@@ -161,58 +161,22 @@ def compute_feats(
 
         for ann in ann_by_image[image_id]:
             if "keypoints" in ann.keys():
-                pose_keypoints = ann['keypoints']
-            
-            elif "confidence" in ann.keys():
-                label_vec.append(act_id_to_str[ann["category_id"]])
-                x, y = ann["bbox"][0], ann["bbox"][1]
-                w, h = ann["bbox"][2], ann["bbox"][3]
-                
-                if aug_trans_range != None and aug_rot_range != None:
-                    
-                    # print(f"performing augmentation")
-                    random_translation_x = np.random.uniform(aug_trans_range[0], aug_trans_range[1])
-                    random_translation_y = np.random.uniform(aug_trans_range[0], aug_trans_range[1])
-                    random_rotation = np.random.uniform(aug_rot_range[0], aug_rot_range[1])
-                    
-                    # print(f"random_translation_x: {random_translation_x}, random_translation_y: {random_translation_y}")
-                    
-                    object_center_x, object_center_y = x + w//2, y + h//2
-                    
-                    rotation_matrix = np.array([[np.cos(random_rotation), -np.sin(random_rotation), random_translation_x], 
-                                                [np.sin(random_rotation), np.cos(random_rotation), random_translation_y],
-                                                [0, 0, 1]])
-                    
-                    # print(f"before xy: {x}, {y}")
-                    
-                    # x += random_translation_x
-                    # y += random_translation_y
-                    
-                    xy = np.array([x, y, 1])
-                    xy_center = np.array([object_center_x, object_center_y, 1])
-                    
-                    rot_xy = (xy-xy_center) @ rotation_matrix.T + xy_center
-                    
-                    # print(f"rot_xy: {rot_xy}")
-                    # rot_xy = np.linalg.
-                    
-                    x = rot_xy[0]
-                    y = rot_xy[1]
-                    
-                    # print(f"after xy: {x}, {y}")
+                # print(f"########### this has keypoints #################")
+                if "left_hand_offset" in ann.keys():
+                    joint_left_hand_offset.append(ann['left_hand_offset'])
+                if "right_hand_offset" in ann.keys():
+                    joint_right_hand_offset.append(ann['right_hand_offset'])
+                if "object_offset" in ann.keys():
+                    joint_object_offset.append(ann['object_offset'])
 
-                    
-                
-                xs.append(x)
-                ys.append(y)
-                ws.append(w)
-                hs.append(h)
+            if "confidence" in ann.keys():
+                label_vec.append(act_id_to_str[ann["category_id"]])
+                xs.append(ann["bbox"][0])
+                ys.append(ann["bbox"][1])
+                ws.append(ann["bbox"][2])
+                hs.append(ann["bbox"][3])
                 label_confidences.append(ann["confidence"])
-                
-                if ann["category_id"] == 5:
-                    num_hands += 1
-                elif ann['category_id'] in [1,2,3,4]:
-                    num_objects += 1
+
                 try:
                     obj_obj_contact_state.append(ann["obj-obj_contact_state"])
                     obj_obj_contact_conf.append(ann["obj-obj_contact_conf"])
@@ -220,87 +184,6 @@ def compute_feats(
                     obj_hand_contact_conf.append(ann["obj-hand_contact_conf"])
                 except KeyError:
                     pass
-                
-        # print(f"pose keyponts: {pose_keypoints}")
-        image_center = 1280//2
-        if num_hands > 0:
-            hands_loc_dict = {}
-            for i, label in enumerate(label_vec):
-                if label == "hand":
-                    hand_center = xs[i] + ws[i]//2
-                    if hand_center < image_center:
-                        if "hands (left)" not in hands_loc_dict.keys():
-                            label_vec[i] = "hands (left)"
-                            hands_loc_dict[label_vec[i]] = (hand_center, i)
-                        else:
-                            if hand_center > hands_loc_dict["hands (left)"][0]:
-                                label_vec[i] = "hands (right)"
-                                hands_loc_dict[label_vec[i]] = (hand_center, i)
-                            else:
-                                prev_index = hands_loc_dict["hands (left)"][1]
-                                label_vec[prev_index] = "hands (right)"
-                                label_vec[i] = "hands (left)"
-                    else:
-                        if "hands (right)" not in hands_loc_dict.keys():
-                            label_vec[i] = "hands (right)"
-                            hands_loc_dict[label_vec[i]] = (hand_center, i)
-                        else:
-                            if hand_center < hands_loc_dict["hands (right)"][0]:
-                                label_vec[i] = "hands (left)"
-                                hands_loc_dict[label_vec[i]] = (hand_center, i)
-                            else:
-                                prev_index = hands_loc_dict["hands (right)"][1]
-                                label_vec[prev_index] = "hands (left)"
-                                label_vec[i] = "hands (right)"
-        zero_offset = [0 for i in range(22)]
-        if (num_hands > 0 or num_objects > 0) and (hands_joints or objects_joints):
-            joint_object_offset = []
-            for i, label in enumerate(label_vec):
-                
-                if hands_joints and num_hands > 0:
-                    
-                    if label == "hands (right)" or label == "hands (left)":
-                        bx, by, bw, bh = xs[i], ys[i], ws[i], hs[i]
-                        hcx, hcy = bx+(bw//2), by+(bh//2)
-                        hand_point = np.array((hcx, hcy))
-                        offset_vector = []
-                        if 'pose_keypoints' in locals():
-                            for joint in pose_keypoints:
-                                jx, jy = joint['xy']
-                                joint_point = np.array((jx, jy))
-                                # print(f"joint_points: {joint_point.dtype}, hand_point: {hand_point.dtype}")
-                                dist = np.linalg.norm(joint_point - hand_point)
-                                offset_vector.append(dist)
-                        else:
-                            offset_vector = zero_offset
-                            
-                        # print(f"offset vector: {offset_vector}")
-                        if label == "hands (left)":
-                        # #     # hcx, hcy = bx+bw//2, by+by//w
-                            joint_left_hand_offset = offset_vector
-                        elif label == "hands (right)":
-                            joint_right_hand_offset = offset_vector
-                            
-                    else:
-                        if objects_joints and num_objects > 0:
-                            bx, by, bw, bh = xs[i], ys[i], ws[i], hs[i]
-                            ocx, ocy = bx+(bw//2), by+(bh//2)
-                            object_point = np.array((ocx, ocy))
-                            offset_vector = []
-                            if 'pose_keypoints' in locals():
-                                for joint in pose_keypoints:
-                                    jx, jy = joint['xy']
-                                    joint_point = np.array((jx, jy))
-                                    # print(f"joint_points: {joint_point.dtype}, object_point: {object_point.dtype}")
-                                    dist = np.linalg.norm(joint_point - object_point)
-                                    offset_vector.append(dist)
-                            else:
-                                offset_vector = zero_offset
-                                
-                            joint_object_offset.append(offset_vector)
-                        # object_offset_wrt = ann_id
-
-                
             
         # print(f"joint_left_hand_offset: {len(joint_left_hand_offset)}")
         # print(f"joint_right_hand_offset: {len(joint_right_hand_offset)}")
@@ -323,43 +206,37 @@ def compute_feats(
             top_n_objects=top_n_objects,
         )
         
-        if objects_joints or hands_joints:
-            zero_offset = [0 for i in range(22)]
-            offset_vector = []
-            if hands_joints:
-                
-                # print(f"joint_left_hand_offset: {len(joint_left_hand_offset)}")
-                # print(f"joint_right_hand_offset: {len(joint_right_hand_offset)}")
-                
-                if len(joint_left_hand_offset) >= 1:
-                    # print(f"joint_left_hand_offset[0]: {joint_left_hand_offset}")
-                    offset_vector.extend(joint_left_hand_offset)
-                else:
-                    offset_vector.extend(zero_offset)
-                
-                if len(joint_right_hand_offset) >= 1:
-                    offset_vector.extend(joint_right_hand_offset)
-                else:
-                    offset_vector.extend(zero_offset)
-            if objects_joints:
-                
-                # print(f"joint_object_offset: {len(joint_object_offset)}")
-                # print(f"joint_object_offset: {joint_object_offset}")
-                
-                for i in range(top_n_objects):
-                    if len(joint_object_offset) > i:
-                        offset_vector.extend(joint_object_offset[i])
-                    else:
-                        # print(f"offset_vector: {offset_vector}")
-                        # print(f"zero_offset: {zero_offset}")
-                        offset_vector.extend(zero_offset)
+        zero_offset = [0 for i in range(22)]
+        # print(len(zero_offset))
+        offset_vector = []
+        if len(joint_left_hand_offset) >= 1:
+            # print(len(joint_left_hand_offset[0]))
+            # feature_vec.append(joint_left_hand_offset[0])
+            offset_vector += joint_left_hand_offset[0]
+        else:
+            # feature_vec.append(zero_offset)
+            offset_vector += zero_offset
+        
+        if len(joint_right_hand_offset) >= 1:
+            # feature_vec.append(joint_right_hand_offset[0])
+            offset_vector += joint_right_hand_offset[0]
+        else:
+            # feature_vec.append(zero_offset)
+            offset_vector += zero_offset
+        
+        if len(joint_object_offset) >= 1:
+            # feature_vec.append(joint_object_offset[0])
+            offset_vector += joint_object_offset[0]
+
+        else:
+            # feature_vec.append(zero_offset)
+            offset_vector += zero_offset
 
             
-            feature_vec.extend(offset_vector)
             
-            
-        # print(f"feature_vec: {feature_vec}")
+        # print(f"offset_vector: {len(offset_vector)}")
         
+        feature_vec += offset_vector
             
         feature_vec = np.array(feature_vec, dtype=np.float64)
         
